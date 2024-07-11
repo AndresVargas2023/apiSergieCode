b914a1e21b06b87a64dbff754fecf8833d4df3cd
Andres Vargas <av6135@gmail.com>
2024-07-11 16:41:24 -0400

    first commit

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.json()) // Parseador de Bodies
 
 //Acá conectaremos la base de datos:
-mongoose.connect(process.env.MONGO_URL, { dbName: process.env.MONGO_DB_NAME })
+mongoose.connect(process.env.MONGODB_URI, { dbName: process.env.MONGO_DB_NAME })
 const db = mongoose.connection;
 
 app.use('/books', bookRoutes)
