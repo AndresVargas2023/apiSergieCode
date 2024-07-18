460988666fcc42176cfd3fe56b5bbb07f0f76bef
Andres Vargas <av6135@gmail.com>
2024-07-18 00:51:30 -0400

    agregando cros al proyecto para permitir consumo de api

--- src/app.js
+++ src/app.js
@@ -1,30 +1,35 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const { config } = require('dotenv')
-config()
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { config } = require('dotenv');
+config();
 
-const bookRoutes = require('./routes/book.routes')
+const bookRoutes = require('./routes/book.routes');
 
 const app = express();
-app.use(bodyParser.json()) // Parseador de Bodies
 
-// Acá conectaremos la base de datos:
+// Habilitar CORS
+app.use(cors());
+
+// Parseador de Bodies
+app.use(bodyParser.json());
+
+// Conexión a la base de datos
 mongoose.connect(process.env.MONGODB_URI, {
   dbName: process.env.MONGO_DB_NAME,
   serverSelectionTimeoutMS: 50000, 
   ssl: true
-  
 })
 .then(() => console.log('Conexión a MongoDB exitosa'))
 .catch(err => console.error('Error al conectar a MongoDB:', err));
 
 const db = mongoose.connection;
 
-app.use('/books', bookRoutes)
+app.use('/books', bookRoutes);
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Servidor iniciado en el puerto ${port}`)
-})
+    console.log(`Servidor iniciado en el puerto ${port}`);
+});
